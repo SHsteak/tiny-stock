73af869e0ae85b009f133965ccf40a795fb74452
김정은 <jeongeun.kim@innowireless.com>
2021-02-18 17:52:53 +0900

    eslint rule 추가

--- view/.eslintrc.js
+++ view/.eslintrc.js
@@ -23,15 +23,17 @@ module.exports = {
      "@typescript-eslint"
   ],
   "rules": {
+    "arrow-spacing": "warn",
     "indent": ["error", 2],
+    "keyword-spacing": "warn",
     "linebreak-style": ["error", "windows"],
     // type definition 목적으로 사용하는 타입 변수를 error로 잡아서 off 처리.
     // 참고: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
-    "object-curly-spacing": ["error", "always"],
-    "quotes": ["error", "double"],
-    "semi": ["error", "always"],
+    "@typescript-eslint/no-unused-vars": "error",
+    "object-curly-spacing": ["warn", "always"],
+    "quotes": ["warn", "double"],
+    "semi": ["warn", "always"],
     "space-before-function-paren": ["warn", "never"]
   }
 };
